70000838acef04919e15cf9e00ebefc02978ef1f
VolodSh <vzujka.com.ua@gmail.com>
2021-02-01 16:01:35 +0200

    Updates

---
+++ precache-manifest.592cdc2817881e5b7fc437123bae8373.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb298684b8b5640e4aab6d60e07ac9c8",
+    "url": "/pb_redux_toolkit/index.html"
+  },
+  {
+    "revision": "19eaed34c9c99d17cad4",
+    "url": "/pb_redux_toolkit/static/css/main.cacfd7e8.chunk.css"
+  },
+  {
+    "revision": "4491d5a9cbc81a1a2339",
+    "url": "/pb_redux_toolkit/static/js/2.4cb1312f.chunk.js"
+  },
+  {
+    "revision": "19eaed34c9c99d17cad4",
+    "url": "/pb_redux_toolkit/static/js/main.5c40c438.chunk.js"
+  },
+  {
+    "revision": "fc7df4762d17b3ccdf6f",
+    "url": "/pb_redux_toolkit/static/js/runtime-main.3264e2fd.js"
+  }
+]);
